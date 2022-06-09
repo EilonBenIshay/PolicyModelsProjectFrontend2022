bd44117b04c06233c181eb5e248822c16f04a8df
Eilon Ben-Ishay <eilon950@gmail.com>
2022-06-09 17:56:38 +0300

    arabic support

--- textAssets.js
+++ textAssets.js
@@ -1,26 +1,26 @@
 export class TextAssets {
     constructor(){
         this.languages = ["English-Raw", "he-IL", "ar-IL","en-US"];
-        this.welcome = ["Welcome", "ברוכים הבאים", "", "Welcome"];
-        this.start_interview = ["Start Interview", "התחל ראיון", "","Start Interview"];
-        this.start = ["Start","התחלה","","Start"];
-        this.restart = ["Restart","התחל מחדש","","Restart"];
-        this.show_transcript = ["Show Transcript","הראה תשובות","","Show Transcript"];
-        this.hide_transcript = ["Hide Transcript","הסתר תשובות", "","Hide Transcript"];
-        this.question = ["Question", "שאלה","","Question"];
-        this.your_answer = ["Your answer","התשובה שלך","","Your answer"];
-        this.revisit = ["Revisit this question", "חזור לשאלה זו","","Revisit this question"];
-        this.show_conclusion = ["Show Conclusion", "הראה תוצאות", "", "Show Conclusion"];
-        this.home = ["Home", "חזרה לעמוד הבית","","Home"];
+        this.welcome = ["Welcome", "ברוכים הבאים", "اهلا وسهلا ", "Welcome"];
+        this.start_interview = ["Start Interview", "התחל ראיון", "ابدأ المقابلة","Start Interview"];
+        this.start = ["Start","התחלה","ابدأ ","Start"];
+        this.restart = ["Restart","התחל מחדש","ابدا مجددا ","Restart"];
+        this.show_transcript = ["Show Transcript","הראה תשובות","اعرض الاجابات","Show Transcript"];
+        this.hide_transcript = ["Hide Transcript","הסתר תשובות", "اخفاء الاجوبه","Hide Transcript"];
+        this.question = ["Question", "שאלה","سؤال","Question"];
+        this.your_answer = ["Your answer","התשובה שלך","جوابك","Your answer"];
+        this.revisit = ["Revisit this question", "חזור לשאלה זו","العوده الى هذا السؤال","Revisit this question"];
+        this.show_conclusion = ["Show Conclusion", "הראה תוצאות", "اعرض النتائج", "Show Conclusion"];
+        this.home = ["Home", "חזרה לעמוד הבית","ارجع لعمود الدار","Home"];
         this.welcome_PM = ["Welcome to the PolicyModels test site!", "ברוכים הבאים לאתר הזמני של PolicyModels","","Welcome to the PolicyModels test site!"];
-        this.results = ["Your results", "התוצאות שלך", "", "your results"];
-        this.conclusion_page = ["Conclusion Page", "עמוד התוצאות","","Conclusion Page"];
+        this.results = ["Your results", "התוצאות שלך", "نتائجك", "your results"];
+        this.conclusion_page = ["Conclusion Page", "עמוד התוצאות","صفحة النتائج","Conclusion Page"];
         this.press_conclusions = ["Press the \"show conclusion\" button to see the conclusion of your interview","לחץ על כפתור \"הראה תוצאות\" על מנת לראות את תוצאות הראיון","","Press the \"show conclusion\" button to see the conclusion of your interview"];
-        this.download_transcript = ["Download Transcript", "הורד גיליון תשובות", "", "Download Transcript"];
-        this.writeFeedback = ["Write Feedback", "כתוב משוב", "", "Write Feedback"];
-        this.submitFeedback = ["Submit Feedback", "שלח משוב", "", "Submit Feedback"];
-        this.show_tags = ["Show Current Tags (intermediate result)", "הראה תוצאות ביניים", "", "Show Current Tags (intermediate result)"];
-        this.hide_tags = ["Hide Current Tags (intermediate result)", "הסתר תוצאות ביניים", "", "Hide Current Tags (intermediate result)"];
-        this.my_feedback_is = ["My Feedback is:", "המשוב שלי הוא:", "", "My Feedback is:"]
+        this.download_transcript = ["Download Transcript", "הורד גיליון תשובות", "حمل كراس الاجوبه ", "Download Transcript"];
+        this.writeFeedback = ["Write Feedback", "כתוב משוב", "اكتب مجدداَ", "Write Feedback"];
+        this.submitFeedback = ["Submit Feedback", "שלח משוב", "ارسل مجددا", "Submit Feedback"];
+        this.show_tags = ["Show Current Tags (intermediate result)", "הראה תוצאות ביניים", "اعرض النتائج", "Show Current Tags (intermediate result)"];
+        this.hide_tags = ["Hide Current Tags (intermediate result)", "הסתר תוצאות ביניים", "اخفي النتائج", "Hide Current Tags (intermediate result)"];
+        this.my_feedback_is = ["My Feedback is:", "המשוב שלי הוא:", "ملاحظتي هي ", "My Feedback is:"]
     }
 }
